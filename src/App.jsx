5a139291ac264bf01c0f29fe4990e326e21cce93
ShaheerFarooqi <shaheerfarooqi56@gmail.com>
2023-10-17 00:11:21 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Navigation from "./component/navigation";
+import HeroSection from "./component/Hero";
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/navigation.jsx
@@ -0,0 +1,25 @@
+const Navigation = () => {
+  return (
+    <nav className="container">
+      <div className="logo">
+        <img src="images/brand_logo.png" alt="" />
+      </div>
+      <ul>
+        <li>
+          <a href="#">MENU</a>
+        </li>
+        <li>
+          <a href="#">LOCATION</a>
+        </li>
+        <li>
+          <a href="#">ABOUT</a>
+        </li>
+        <li>
+          <a href="#">CONTACT</a>
+        </li>
+      </ul>
+      <button>login</button>
+    </nav>
+  );
+};
+export default Navigation;
